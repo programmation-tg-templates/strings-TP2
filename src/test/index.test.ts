2e2d18a9477834b96ebf58c41aa6b42a0fdf6f90
François Roland <francois.roland@heh.be>
2025-10-15 14:56:05 +0200

    Fix test description for Mademoiselle password generation

--- src/test/index.test.ts
+++ src/test/index.test.ts
@@ -5,7 +5,7 @@ describe("Générateur de mot de passe", () => {
     expect(genererMotDePasse("TypeScript")).toEqual("TyP3ScR1Pt10");
   });
 
-  test("Mademoiselle", () => {
-    expect(genererMotDePasse("Générer un mot de passe à partir de Mademoiselle")).toEqual("M@D3M01s3lL312");
+  test("Générer un mot de passe à partir de Mademoiselle", () => {
+    expect(genererMotDePasse("Mademoiselle")).toEqual("M@D3M01s3lL312");
   });
 });
